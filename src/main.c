edfbc7161ce0fac6a01a9c470da583f5596b3254
raddari <harveywyatt@hotmail.com>
2022-02-07 17:02:36 +1030

    Add additional flags to raw mode

--- src/main.c
+++ src/main.c
@@ -1,5 +1,7 @@
 #include "terminal.h"
 
+#include <ctype.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 
@@ -9,7 +11,13 @@ int main() {
   atexit(term_restore);
 
   char c;
-  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+    if (iscntrl(c)) {
+      printf("%d\n\r", c);
+    } else {
+      printf("%d ('%c')\n\r", c, c);
+    }
+  }
 
   return EXIT_SUCCESS;
 }
--- src/terminal.c
+++ src/terminal.c
@@ -10,7 +10,14 @@ static struct termios original_termios;
 void term_enable_raw_mode() {
   tcgetattr(STDIN_FILENO, &original_termios);
   struct termios raw = original_termios;
-  raw.c_lflag &= ~(ECHO | ICANON);
+  // Miscellaneous flags for traditional "raw mode"
+  raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+  // Disable output formatting (\n -> \n\r)
+  raw.c_oflag &= ~(OPOST);
+  // Set character size to 8 bits per byte (probably the default anyway)
+  raw.c_cflag |= (CS8);
+  // Disable echo, line reading, <C-v> and interrupt signals
+  raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
 
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
