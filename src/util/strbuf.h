a49dfa5ec05b1c91bda428bf9fa806d0320b1414
raddari <harveywyatt@hotmail.com>
2022-02-10 21:55:46 +1030

    Fix memory leaks with strbuf

--- src/util/strbuf.h
+++ src/util/strbuf.h
@@ -1,11 +1,12 @@
 #pragma once
 
 
-typedef struct StrBuf StrBuf;
+typedef struct StrBuf {
+  char *buffer;
+  int len;
+} StrBuf;
 
 
-StrBuf* str_buf_new();
 void str_buf_destroy(StrBuf *str_buf);
-void str_buf_append(StrBuf *str_buf, const char *bytes, int len);
-void str_buf_append_str(StrBuf *str_buf, const char *str);
-int str_buf_len(StrBuf *str_buf);
+void str_buf_append(StrBuf *str_buf, const char *str);
+void str_buf_append_bytes(StrBuf *str_buf, const char *bytes, int len);
